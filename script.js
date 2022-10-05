afb01db6c1967419b4160e7da32091e9a6d58337
joel-dev22 <joeltmathew1999@gmail.com>
2022-10-05 09:03:15 +0530

    learned about async await

--- script.js
+++ script.js
@@ -75,31 +75,31 @@
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-// const btn = document.querySelector('.btn-country');
-// const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-// const renderCountry = function (data, className = '') {
-//   const html = `
-//     <article class="country ${className}">
-//       <img class="country__img" src="${data.flag}" />
-//         <div class="country__data">
-//           <h3 class="country__name">${data.name}</h3>
-//           <h4 class="country__region">${data.region}</h4>
-//           <p class="country__row"><span>👫</span>${(
-//             +data.population / 1000000
-//           ).toFixed(1)} people</p>
-//           <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//           <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
-//         </div>
-//     </article>
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+        </div>
+    </article>
 
-//   `;
+  `;
 
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 // const renderError = function (msg) {
 //   countriesContainer.insertAdjacentText('beforeend', msg);
@@ -287,16 +287,123 @@
 //     console.log('I waited for 1 seconds');
 //   });
 
-const getPromise = function () {
+// const getPromise = function () {
+//   return new Promise(function (resolve, reject) {
+//     // navigator.geolocation.getCurrentPosition(
+//     //   position => resolve(position),
+//     //   err => reject(err)
+//     // );
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// };
+
+// getPromise()
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err));
+
+// Coding Challenge
+
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const div = document.createElement('div');
+//     const img = document.createElement('img');
+//     div.appendChild(img);
+//     img.src = imgPath;
+
+//     resolve(document.getElementById('images').appendChild('div'));
+//     reject('Image not loaded');
+//   });
+// };
+
+// createImage(
+//   'https://www.google.com/search?q=garden+picture&sxsrf=ALiCzsYsGnvcyWfAJgGfoxn-swKePH4lag:1664884415295&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjNuanhwcb6AhVrcGwGHW23CIQQ_AUoAXoECAUQAw&biw=1366&bih=657&dpr=1#imgrc=A1B47CEdV3sM2M'
+// );
+
+/*
+const wait = seconds => {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imagesContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
-    // navigator.geolocation.getCurrentPosition(
-    //   position => resolve(position),
-    //   err => reject(err)
-    // );
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imagesContainer.append('img');
+      resolve(img);
+    });
+    img.addEventListener('error', () => {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Img1 is loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image2 is loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err))
+  .finally(() => {
+    console.log('Good luck');
+  });
+*/
+
+// Async Await
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-getPromise()
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const whereAmI = async function () {
+  try {
+    const pos = await getPosition();
+
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    const geo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!geo.ok) throw new Error('Problem with fetching coordinates...');
+    const dataGeo = await geo.json();
+    console.log(dataGeo.country);
+
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+    );
+    if (!res.ok) throw new Error('Problem with fetching country');
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(err);
+    renderError(`${err.message}`);
+  }
+};
+
+whereAmI();
